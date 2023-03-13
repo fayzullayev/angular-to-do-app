8ae73d0a483815f617cacbbdadea2a0db6e3ecd9
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-03-13 15:10:48 +0500

    components have created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ToDoAddFormComponent } from './components/to-do-add-form/to-do-add-form.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { ToDoItemComponent } from './components/to-do-item/to-do-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToDoAddFormComponent,
+    ToDoListComponent,
+    ToDoItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/to-do-add-form/to-do-add-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-add-form',
+  templateUrl: './to-do-add-form.component.html',
+  styleUrls: ['./to-do-add-form.component.css']
+})
+export class ToDoAddFormComponent {
+
+}
