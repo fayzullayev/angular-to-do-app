55bb4dc8bd10a3d55331f1d73a23fb627fab3808
Mirodil Fayzullayev <odilfayz22@gmail.com>
2023-03-13 15:29:05 +0500

    .

--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToDoList } from '../../types/to-do-list';
+import { ToDoListServices } from '../../services/to-do-list-services';
 
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.css'],
 })
-export class ToDoListComponent {}
+export class ToDoListComponent implements OnInit {
+  public todos: ToDoList[] = [];
+
+  constructor(private todoService: ToDoListServices) {}
+
+  ngOnInit() {
+    this.todos = this.todoService.getTodos;
+  }
+}
